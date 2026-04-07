e81a1015d2bb9314f2fb8337dccedcb946efa66d
Arne Van dycke <avdycke11@gmail.com>
2026-04-07 22:10:12 +0200

    legende 10

--- opdracht10.m
+++ opdracht10.m
@@ -37,8 +37,8 @@ yl = ylim;
 dy = yl(2) - yl(1);
 t1_plot = t1(t1 >= min(xplot) & t1 <= max(xplot));
 t2_plot = t2(t2 >= min(xplot) & t2 <= max(xplot));
-plot(t1_plot, (yl(1) - 0.05*dy) * ones(size(t1_plot)), 'bv', 'MarkerSize', 4, 'HandleVisibility', 'off');
-plot(t2_plot, (yl(1) - 0.10*dy) * ones(size(t2_plot)), 'rv', 'MarkerSize', 4, 'HandleVisibility', 'off');
+plot(t1_plot, (yl(1) - 0.05*dy) * ones(size(t1_plot)), 'bv', 'MarkerSize', 4, 'DisplayName', 'knopen t1');
+plot(t2_plot, (yl(1) - 0.10*dy) * ones(size(t2_plot)), 'rv', 'MarkerSize', 4, 'DisplayName', 'knopen t2');
 ylim([yl(1) - 0.15*dy, yl(2)]);
 
 grid on;
@@ -49,31 +49,33 @@ legend('Location', 'best');
 % figuur waar ze apart staan
 figure;
 subplot(2,1,1);
-plot(x, f, 'ko', 'MarkerSize', 4); hold on;
-plot(xplot, Z1, 'b-', 'LineWidth', 1.5);
+plot(x, f, 'ko', 'MarkerSize', 4, 'DisplayName', 'meetpunten'); hold on;
+plot(xplot, Z1, 'b-', 'LineWidth', 1.5, 'DisplayName', 'fit met t1');
 
 yl = ylim;
 dy = yl(2) - yl(1);
 t1_plot = t1(t1 >= min(xplot) & t1 <= max(xplot));
-plot(t1_plot, (yl(1) - 0.05*dy) * ones(size(t1_plot)), 'bv', 'MarkerSize', 4);
+plot(t1_plot, (yl(1) - 0.05*dy) * ones(size(t1_plot)), 'bv', 'MarkerSize', 4, 'DisplayName', 'knopen t1');
 ylim([yl(1) - 0.10*dy, yl(2)]);
 
 grid on;
 title('Splinefit met knopenrij t1');
+legend('Location', 'northwest');
 xlabel('x'); ylabel('f(x)');
 
 subplot(2,1,2);
-plot(x, f, 'ko', 'MarkerSize', 4); hold on;
-plot(xplot, Z2, 'r--', 'LineWidth', 1.5);
+plot(x, f, 'ko', 'MarkerSize', 4, 'DisplayName', 'datapunten'); hold on;
+plot(xplot, Z2, 'r--', 'LineWidth', 1.5, 'DisplayName', 'fit met t2');
 
 yl = ylim;
 dy = yl(2) - yl(1);
 t2_plot = t2(t2 >= min(xplot) & t2 <= max(xplot));
-plot(t2_plot, (yl(1) - 0.05*dy) * ones(size(t2_plot)), 'rv', 'MarkerSize', 4);
+plot(t2_plot, (yl(1) - 0.05*dy) * ones(size(t2_plot)), 'rv', 'MarkerSize', 4, 'DisplayName', 'knopen t2');
 ylim([yl(1) - 0.10*dy, yl(2)]);
 
 grid on;
 title('Splinefit met knopenrij t2');
+legend('Location', 'northwest');
 xlabel('x'); ylabel('f(x)');
 
 % kwantitatieve vergelijking van de fout
